eb75a0b5c851093d5857e9bc2edc20e9c36e2214
Gabriel Coman <dev.gabriel.coman@gmail.com>
2017-09-23 15:48:08 +0100

    More reports

--- mgmt/sql/reports.sql
+++ mgmt/sql/reports.sql
@@ -1,56 +1,57 @@
+use boxxit;
+
+create view Report_TotalUsers as
+select
+ count(userId) as Total_Users
+from Users;
+
 CREATE VIEW Report_UserStatus as
 SELECT
 	a.name as Name,
-	if (a.email <> '', a.email, 'Not available') as `Email address`,
-  	if (b.NrCategs <> '', b.NrCategs, '0') as `Available likes`,
-	if (a.birthday <> '', a.birthday, 'Not available') as Birthday,
-	a.joinDate as `Joining date`
+	if (a.email <> '', a.email, 'Not available') as `Email_Address`,
+  	if (b.NrCategs <> '', b.NrCategs, '0') as `Available_Likes`,
+	if (a.birthday <> '', date_format(a.birthday, '%d %b %Y'), 'Not available') as Birthday,
+	date_format(a.joinDate, '%d %b %Y') as `Joining_Date`
 FROM Users a
 LEFT JOIN (
-  SELECT
-    a.userId,
-    COUNT(a.categId) as NrCategs
+	SELECT
+		a.userId,
+		COUNT(a.categId) as NrCategs
    FROM `UserCategories` a
-   left join `Categories` b on a.categId = b.categId
+   LEFT JOIN `Categories` b on a.categId = b.categId
    WHERE b.isValuable = TRUE
    GROUP BY a.userId
-) b	on a.userId = b.userId
+) b on a.userId = b.userId;
 
-CREATE VIEW Report_CategoryProducts as
-SELECT
-	b.name as `Like`,
-  b.category as `Facebook category`,
-  b.searchIndex as `Amazon category`,
-  COUNT(a.asin) as `Available products`
-FROM `CategoriesProducts` a
-LEFT JOIN `Categories` b on a.categId = b.categId
-GROUP BY a.categId
+create view Report_NewJoiners as
+select
+ date_format(joinDate, "%d %b %Y") as Join_Date,
+ count(userId) as Joiners
+from Users
+group by Join_Date;
 
-CREATE VIEW Report_Products as
-SELECT
-	c.name as `Like`,
-    a.title as `Product`,
-    if (a.price <> '', a.price, '£0.00') as `Price`
-FROM `Products` a
-LEFT JOIN `CategoriesProducts` b on a.asin = b.asin
-LEFT JOIN `Categories` c on b.categId = c.categId
+create view Report_NewJoiners_Today as
+select
+ count(joinDate) as Joiners_Today
+from Users a
+where date_format(joinDate, '%d %b %Y') = date_format(now(), '%d %b %Y');
 
 CREATE VIEW Report_Friends as
 SELECT
 	b.name as `User`,
-  c.name AS `Is friends with`
+  c.name AS `Is_Friends_With`
 FROM `Friends` a
 LEFT JOIN Users b on a.user1Id = b.userId
 LEFT JOIN Users c on a.user2Id = c.userId
-WHERE b.name <> '' and c.name <> ''
+WHERE b.name <> '' and c.name <> '';
 
 CREATE VIEW Report_Birthdays as
 SELECT
-	c.name as `Should notify`,
-    a.name AS `Friend birthday`,
-    a.daysTillBirthday as `Days until birthday`,
-    if(c.token <> '', 'YES', 'NO') as `Can send notification`,
-    IF(c.email <> '', 'YES', 'NO') as `Can send email`
+	c.name as `Should_Notify`,
+    a.name AS `Friend_Birthday`,
+    a.daysTillBirthday as `Days_Until_Birthday`,
+    if(c.token <> '', 'YES', 'NO') as `Can_Send_Notification`,
+    IF(c.email <> '', 'YES', 'NO') as `Can_Send_Email`
 FROM
 	(SELECT
 		userId,
@@ -59,3 +60,22 @@ FROM
 	FROM boxxit.Users where (dayofyear(birthday) - dayofyear(now())) BETWEEN 0 AND 100) a
 LEFT JOIN boxxit.Friends b on a.userId = b.user1Id
 LEFT JOIN boxxit.Users c on b.user2Id = c.userId;
+
+CREATE VIEW Report_CategoryProducts as
+SELECT
+	b.name as `Category`,
+	b.category as `Facebook_Category`,
+	b.searchIndex as `Amazon_Category`,
+	COUNT(a.asin) as `Available_Products`
+FROM `CategoriesProducts` a
+LEFT JOIN `Categories` b on a.categId = b.categId
+GROUP BY a.categId;
+
+CREATE VIEW Report_Products as
+SELECT
+	c.name as `Category`,
+    a.title as `Product`,
+    if (a.price <> '', a.price, '£0.00') as `Price`
+FROM `Products` a
+LEFT JOIN `CategoriesProducts` b on a.asin = b.asin
+LEFT JOIN `Categories` c on b.categId = c.categId;
