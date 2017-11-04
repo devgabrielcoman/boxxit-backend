e9c45ccaf690f9bf8ea6215a4e9b285c3cede32b
Gabriel Coman <dev.gabriel.coman@gmail.com>
2017-11-04 14:43:04 +0000

    Update

--- mgmt/sql/get_favourites.sql
+++ mgmt/sql/get_favourites.sql
@@ -3,15 +3,17 @@ DELIMITER //
 CREATE PROCEDURE getFavouriteProductsForUser(in userId varchar(80))
 select
 	a.asin,
-  b.title as title,
+    b.title as title,
 	b.amount as amount,
 	b.price as price,
 	b.click as click,
 	b.smallImage as smallIcon,
-	b.bigImage as largeIcon
+	b.bigImage as largeIcon,
+    true as isFavourite
 from boxxit.Favourites a
 left join boxxit.Products b on a.asin = b.asin
 where
 	a.userId = userId;
 END //
 DELIMITER ;
+ 
